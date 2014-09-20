6dd777ce6e0a0cffd67dfc45787794c9a185787e
d.mueller <d.mueller@homelabs.de>
2014-09-20 17:44:53 +0200

    correct path

--- workbench_api/src/main/java/de/homelabs/webapps/workbench/domain/WorkbenchConstants.java
+++ workbench_api/src/main/java/de/homelabs/webapps/workbench/domain/WorkbenchConstants.java
@@ -2,5 +2,5 @@ package de.homelabs.webapps.workbench.domain;
 
 public class WorkbenchConstants {
 
-	public final static String TEMPLATE_PATH = "de/homelabs/webapps/workbench/menu/";
+	public final static String TEMPLATE_PATH = "de/homelabs/webapps/workbench/templates/";
 }
